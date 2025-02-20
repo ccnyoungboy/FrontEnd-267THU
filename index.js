c20d07a93bcc11a7e5a9157f9bcc8c983666fe13
Chaichana Toomjan <ccnyoungboy@gmail.com>
2025-02-20 09:16:25 +0700

    cm

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const axios = require("axios");
+var bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
+
+app.set("views", path.join(__dirname, "/public/views"));
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
